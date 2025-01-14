63f89422b3e326228d66914a3e1dc6c80211ddd0
ehsan <ehsan.taabbodi@gmail.com>
2025-01-14 15:29:20 -0500

    add quickSort and insertionSort

--- dsa-sorting/insertion.js
+++ dsa-sorting/insertion.js
@@ -1,3 +1,26 @@
-function insertionSort() {}
+function insertionSort(arr) {
+    for (i = 1; i < arr.length; i++) {
+        let currentVal = arr[i]
+        let j = i - 1
+
+        while (j >= 0 && arr[j] > currentVal) {
+            arr[j + 1] = arr[j]
+            j--
+        }
+        arr[j + 1] = currentVal;
+    }
+    return arr
+
+
+}
 
 module.exports = insertionSort;
+
+
+// Time Complexity
+// Best case:
+// O(n)(already sorted array)
+// Worst case:
+// O(n2)(reverse sorted array)
+// Average case:
+// O(n2)
--- dsa-sorting/quick.js
+++ dsa-sorting/quick.js
@@ -3,12 +3,43 @@ pivot accepts an array, starting index, and ending index
 You can assume the pivot is always the first element
 */
 
-function pivot(){}
+function pivot() { }
 
 /*
 quickSort accepts an array, left index, and right index
 */
 
-function quickSort() {}
+function quickSort(arr) {
+    if (arr.length <= 1) return arr
+
+    let pivot = arr[Math.floor(arr.length / 2)]
+    const less = []
+    const equal = []
+    const greater = []
+
+    for (let element of arr) {
+        if (element < pivot) {
+            less.push(element)
+        } else if (element === pivot) {
+            equal.push(element)
+        } else {
+            greater.push(element)
+        }
+    }
+
+    return [...quickSort(less), ...equal, quickSort(greater)]
+}
 
 module.exports = quickSort;
+
+// Time Complexity
+// Best / Average Case:
+// 𝑂(𝑛 log⁡ 𝑛)
+// O(n log n)
+// Worst Case:
+// 𝑂(𝑛2)
+// O(n2)(occurs when the pivot is poorly chosen)
+// Space Complexity
+// Average Case:
+// 𝑂(log𝑛)
+// O(logn) due to recursive function calls.
